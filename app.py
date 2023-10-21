ad673540683f8c264283c8c7272dcf756aed5f05
Srihari Srinivasan <118172807+speedwagon1299@users.noreply.github.com>
2023-10-21 10:05:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, jsonify
 import subprocess
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -10,16 +11,15 @@ def index():
 @app.route('/get_prediction')
 def get_prediction():
     try:
-        # Run the Python script using subprocess.run
-        result = subprocess.run(['python', 'API_final.py'], capture_output=True, text=True, check=True)
-        
+        # Run the Python script and capture its output
+        result = subprocess.check_output(['python API_final.py'], universal_newlines=True)
+
         # Format the output as HTML
-        formatted_result = f'<h2>5 Hours Data:</h2>\n<p>{result.stdout}</p>'
+        formatted_result = f'<h2>5 Hours Data:</h2>\n<p>{result}</p>'
+
         return formatted_result
-    except subprocess.CalledProcessError as e:
-        return f'<h2>Error:</h2>\n<p>{e.stderr}</p>'
     except Exception as e:
-        return f'<h2>Error:</h2>\n<p>{str(e)}'
+        return f'<h2>Error:</h2>\n<p>{str(e)}</p>'
 
 if __name__ == '__main__':
-    app.run(debug=False)  # Debug should be False in a production environment
+    app.run(host="0.0.0.0",port=5000)
